9423712f9cce312c4b6be0fb777880be4fb1ba4d
AnastasiyaPlotnikova <world771@yandex.ru>
2022-11-07 19:47:49 +0500

    first zadacha10 added

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number <= 999)
+{
+    int s = (number / 10) % 10;
+    Console.Write("Вторая цифра трехзначного числа:" + s);
+}
+else
+{
+    Console.WriteLine("Введенное число не трехзначное");
+}
