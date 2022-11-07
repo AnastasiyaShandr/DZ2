c4e582e79ede908238a00327c6767a6994ff02d8
AnastasiyaPlotnikova <world771@yandex.ru>
2022-11-07 21:25:39 +0500

    first zadacha15 added

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите число дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1 | number < 7)
+{
+    switch (number)
+    {
+        case 1:
+            Console.WriteLine("Это не выходной");
+            break;
+        case 2:
+            Console.WriteLine("Это не выходной");
+            break;
+        case 3:
+            Console.WriteLine("Это не выходной");
+            break;
+        case 4:
+            Console.WriteLine("Это не выходной");
+            break;
+        case 5:
+            Console.WriteLine("Это не выходной");
+            break;
+        case 6:
+            Console.WriteLine("Да, это выходной!");
+            break;
+        case 7:
+            Console.WriteLine("Да, это выходной!");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Не соответствует ни одному дню недели");
+}
